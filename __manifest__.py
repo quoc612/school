d4218b2f5d109223644d5fb4e994e1484763f273
Nguyen Van Quoc <quoc.nguyenvan@vti.com.vn>
2022-07-26 16:26:04 +0700

    school

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+
+    'name': 'School',
+
+    'version': '12.0.1.0.0',
+
+    'summary': 'School Manager',
+
+    'category': 'Tools',
+
+    'author': 'f',
+
+    'maintainer': 'f',
+
+    'company': 'f',
+
+    'website': 'abc.com',
+
+    'depends': ['base','mail'],
+
+    'data': [
+        'views/student_view.xml',
+        'security/ir.model.access.csv'
+    ],
+
+    'images': [],
+
+    'license': 'AGPL-3',
+
+    'installable': True,
+
+    'application': False,
+
+    'auto_install': False,
+}
---
+++ models/student.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class StudentStudent(models.Model):
+    _name = 'student.student'
+    _inherit = 'mail.thread'
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    photo = fields.Binary(string='Image')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
+    student_dob = fields.Date(string="Date of Birth")
+    nationality = fields.Many2one('res.country', string='Nationality')
