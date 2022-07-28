0fe8db043c573cdd32d49469901c40658c8e42fd
Nguyen Van Quoc <quoc.nguyenvan@vti.com.vn>
2022-07-28 14:37:03 +0700

    add kanban

--- models/student.py
+++ models/student.py
@@ -4,8 +4,8 @@ from odoo import models, fields
 class StudentStudent(models.Model):
     _name = 'student.student'
     _inherit = 'mail.thread'
-    name = fields.Char(string="Name", required=True)
-    age = fields.Integer(string="Age")
+    name = fields.Char(string="Name", required=True, track_visibility='always')
+    age = fields.Integer(string="Age", track_visibility='onchange')
     photo = fields.Binary(string='Image')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
     student_dob = fields.Date(string="Date of Birth")
